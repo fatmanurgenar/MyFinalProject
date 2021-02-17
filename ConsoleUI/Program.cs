fc6522f7616e94996429c96a811788a1cbfe934d
FATMANUR GENAR <fatmanur.genar.98@gmail.com>
2021-02-17 23:57:41 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -16,7 +16,7 @@ namespace ConsoleUI
 
         }
 
-        private static void CategoryTest()
+      /*  private static void CategoryTest()
         {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
@@ -24,15 +24,28 @@ namespace ConsoleUI
                 Console.WriteLine(category.CategoryName);
             }
         }
-
+      */
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName );
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            foreach (var product in productManager.GetProductDetails().Data)
             {
                 Console.WriteLine(product.ProductName + "/" + product.CategoryName);
 
             }
         }
+      
     }
 }
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //hangi tipi döndüreceğini bana söyle --> <T>
+    public interface IDataResult<T>:IResult
+    {
+        //IReesulttaki mesajlar dışında birde T türünde datamız olacak bu classta 
+        T Data { get; }
+    }
+}
